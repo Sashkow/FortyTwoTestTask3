99292d028db71fb898222a3eab9f52b9e5b3d784
Sashkow <lykhenko.olexandr@gmail.com>
2015-04-07 15:02:10 +0300

    updated settings

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -169,3 +169,35 @@ TEMPLATE_CONTEXT_PROCESSORS =  (
     )
 
 APPEND_SLASH = True
+
+
+# Specify the selenium test runner
+SELENIUM_TEST_RUNNER = 'django_selenium.selenium_runner.SeleniumTestRunner'
+
+SELENIUM_TIMEOUT = 120
+SELENIUM_DRIVER_TIMEOUT = 10
+# Specify max waiting time for server to finish processing request and deactivates
+SELENIUM_TEST_SERVER_TIMEOUT = 300
+
+#------------------ LOCAL ----------------------------------
+SELENIUM_TESTSERVER_HOST =  'localhost'
+SELENIUM_TESTSERVER_PORT = 8011
+SELENIUM_HOST = None
+SELENIUM_PORT = 4444
+
+SELENIUM_DISPLAY = ':0'
+# Set the drivers that you want to run your tests against
+SELENIUM_DRIVER = 'Firefox'
+SELENIUM_DRIVER_OPTS = dict()
+# ------------------------------------------------------------
+
+#----------------- REMOTE ------------------------------------
+# YOU SHOULD SET THESE IN YOUR LOCAL SETTINGS FILE
+# Path to selenium-server JAR,
+# for example: "/home/dragoon/myproject/selenium-server/selenium-server.jar"
+SELENIUM_PATH= None
+SELENIUM_CAPABILITY = 'FIREFOX'
+SELENIUM_DRIVER = 'Remote'
+SELENIUM_HOST = 'selenium-hub.example.com'
+SELENIUM_TESTSERVER_HOST = '0.0.0.0'
+#------------------------------------------------------------

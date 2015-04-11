01ad11557f90ff24b4af30a022dfe5ed41debe4f
Sashkow <lykhenko.olexandr@gmail.com>
2015-04-11 11:54:36 +0300

    modified makefile 10

--- Makefile
+++ Makefile
@@ -3,10 +3,8 @@ SETTINGS=fortytwo_test_task.settings
 
 test:
 	java -jar selenium-server-standalone-2.45.0.jar&
-	ps -e
-	killall java
-	killall java
-	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$(SETTINGS) $(MANAGE) test
+	
+	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$(SETTINGS) $(MANAGE) test hello.tests.tests_selenium
 
 
 run:
--- apps/hello/tests/tests_selenium.py
+++ apps/hello/tests/tests_selenium.py
@@ -1,97 +1,109 @@
 from django.test import LiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
+# from selenium.webdriver.firefox.webdriver import WebDriver
 
+from selenium import webdriver
 
-class MyLiveServerTestCase(LiveServerTestCase):    
-    """
-    BaseCleass for my selenium test cases
-    """
+class LoginTestCase(LiveServerTestCase):
     fixtures = ['test_data.json']
 
-    def setUp(self):
-        self.driver = WebDriver()
-        self.url = self.live_server_url    
+    def test_small(self):
+        browser = webdriver.Remote(
+            desired_capabilities=webdriver.DesiredCapabilities.FIREFOX,
+            command_executor='http://127.0.0.1:4444/wd/hub'
+        )
+        browser.get('http://seleniumhq.org/')
 
-    def tearDown(self):
 
-        self.driver.quit()
+# class MyLiveServerTestCase(LiveServerTestCase):    
+#     """
+#     BaseCleass for my selenium test cases
+#     """
+#     fixtures = ['test_data.json']
+
+#     def setUp(self):
+#         self.driver = WebDriver()
+#         self.url = self.live_server_url    
+
+#     def tearDown(self):
+
+#         self.driver.quit()
 
 
-class LoginTest(MyLiveServerTestCase):
-    """
-    Test the following user story:
-      - go to main page
-      - go to edit page
-      - edit data
-      - return to main page
-      - see data changed 
-    """
+# class LoginTest(MyLiveServerTestCase):
+#     """
+#     Test the following user story:
+#       - go to main page
+#       - go to edit page
+#       - edit data
+#       - return to main page
+#       - see data changed 
+#     """
 
-    def test_valid_data(self):
-        """
-        test when user enters correct data
-        """
-        # import pdb; pdb.set_trace()
-        self.driver.get(self.url)
+#     def test_valid_data(self):
+#         """
+#         test when user enters correct data
+#         """
+#         # import pdb; pdb.set_trace()
+#         self.driver.get(self.url)
 
         
-        assert "Main Page" in self.driver.title
-        elem = self.driver.find_element_by_name("login_page_link")
-        elem.click()
-        assert "Login Page" in self.driver.title
+#         assert "Main Page" in self.driver.title
+#         elem = self.driver.find_element_by_name("login_page_link")
+#         elem.click()
+#         assert "Login Page" in self.driver.title
 
-        elem = self.driver.find_element_by_id("id_username")
-        elem.clear()
-        elem.send_keys("leela")
+#         elem = self.driver.find_element_by_id("id_username")
+#         elem.clear()
+#         elem.send_keys("leela")
 
-        elem = self.driver.find_element_by_id("id_password")
-        elem.clear()
-        elem.send_keys("leela")
+#         elem = self.driver.find_element_by_id("id_password")
+#         elem.clear()
+#         elem.send_keys("leela")
 
-        elem = self.driver.find_element_by_name("login_submit")
-        elem.click()
-        assert "Main Page" in self.driver.title
-        assert "Name: Leela" in self.driver.page_source
+#         elem = self.driver.find_element_by_name("login_submit")
+#         elem.click()
+#         assert "Main Page" in self.driver.title
+#         assert "Name: Leela" in self.driver.page_source
 
-class AdminEditFormTest(MyLiveServerTestCase):
-    """
-    Test the following user story:
-      - go to main page
-      - go to edit page
-      - edit data
-      - return to main page
-      - see data changed 
-    """
+# class AdminEditFormTest(MyLiveServerTestCase):
+#     """
+#     Test the following user story:
+#       - go to main page
+#       - go to edit page
+#       - edit data
+#       - return to main page
+#       - see data changed 
+#     """
 
-    def test_valid_data(self):
-        """
-        test when user enters correct data
-        """
-        self.driver.get(self.url)
+#     def test_valid_data(self):
+#         """
+#         test when user enters correct data
+#         """
+#         self.driver.get(self.url)
 
-        assert "Main Page" in self.driver.title
-        elem = self.driver.find_element_by_name("edit_page_link")
-        elem.click()
-        assert "Edit Page" in self.driver.title
-        elem = self.driver.find_element_by_id("id_name")
-        elem.clear()
-        elem.send_keys("NewValue")
-        elem = self.driver.find_element_by_name("save_and_back_submit")
-        elem.click()
-        # assert ("..." in elem.get_attribute("value")) or \
-          # (":)" in elem.get_attribute("value"))
+#         assert "Main Page" in self.driver.title
+#         elem = self.driver.find_element_by_name("edit_page_link")
+#         elem.click()
+#         assert "Edit Page" in self.driver.title
+#         elem = self.driver.find_element_by_id("id_name")
+#         elem.clear()
+#         elem.send_keys("NewValue")
+#         elem = self.driver.find_element_by_name("save_and_back_submit")
+#         elem.click()
+#         # assert ("..." in elem.get_attribute("value")) or \
+#           # (":)" in elem.get_attribute("value"))
         
 
-    # def test_invalid_data(self):
-    #     """ test when user enters INcorrect data """
-    #     self.driver.get(self.url)
-    #     assert "Main Page" in self.driver.title
-    #     elem = self.driver.find_element_by_name("edit_page_link")
-    #     elem.click()
-    #     assert "Edit Page" in self.driver.title
-    #     elem = self.driver.find_element_by_id("id_email")
-    #     elem.clear()
-    #     elem.send_keys("BadEmail")
-    #     elem = self.driver.find_element_by_name("save_and_back_submit")
-    #     elem.click()
-    #     assert "Edit Page" in self.driver.title
+#     # def test_invalid_data(self):
+#     #     """ test when user enters INcorrect data """
+#     #     self.driver.get(self.url)
+#     #     assert "Main Page" in self.driver.title
+#     #     elem = self.driver.find_element_by_name("edit_page_link")
+#     #     elem.click()
+#     #     assert "Edit Page" in self.driver.title
+#     #     elem = self.driver.find_element_by_id("id_email")
+#     #     elem.clear()
+#     #     elem.send_keys("BadEmail")
+#     #     elem = self.driver.find_element_by_name("save_and_back_submit")
+#     #     elem.click()
+#     #     assert "Edit Page" in self.driver.title
